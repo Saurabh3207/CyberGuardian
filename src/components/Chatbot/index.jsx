476b5d2a0b965f520803d9e07224818d4cb68706
Krishnaraj Thadesar <krishnaraj.kpt@outlook.com>
2023-12-06 03:17:07 +0530

    done with chat bot css

--- src/components/Chatbot/index.jsx
+++ src/components/Chatbot/index.jsx
@@ -8,104 +8,118 @@ import botAvatar from "../assets/images/bot.png";
 import svgImage from "../assets/images/bot.svg";
 
 function Chatbot() {
-  const [input, setInput] = useState("");
-  const [messages, setMessages] = useState([
-    {
-      message: "Hello, I'm your Chatbot! Ask me anything.",
-      sender: "Chatbot"
-    }
-  ]);
+	const [input, setInput] = useState("");
+	const [messages, setMessages] = useState([
+		{
+			message: "Hello, I'm your Chatbot! Ask me anything.",
+			sender: "Chatbot",
+		},
+	]);
 
-  const handleChange = event => {
-    setInput(event.target.value);
-  };
+	const handleChange = (event) => {
+		setInput(event.target.value);
+	};
 
-  const handleSend = async event => {
-    event.preventDefault();
+	const handleSend = async (event) => {
+		event.preventDefault();
 
-    const newMessage = {
-      message: input,
-      sender: "user"
-    };
+		const newMessage = {
+			message: input,
+			sender: "user",
+		};
 
-    const newMessages = [...messages, newMessage];
-    setMessages(newMessages);
+		const newMessages = [...messages, newMessage];
+		setMessages(newMessages);
 
-    setInput("");
+		setInput("");
 
-    try {
-      const response = await axios.post("http://localhost:5000/predict", {
-        user_input: input
-      });
+		try {
+			const response = await axios.post("http://localhost:5000/predict", {
+				user_input: input,
+			});
 
-      const botResponse = response.data.bot_response;
+			const botResponse = response.data.bot_response;
 
-      setMessages([
-        ...newMessages,
-        {
-          message: botResponse,
-          sender: "Chatbot"
-        }
-      ]);
-    } catch (error) {
-      console.error("Error communicating with Flask API:", error.message);
-    }
-  };
+			setMessages([
+				...newMessages,
+				{
+					message: botResponse,
+					sender: "Chatbot",
+				},
+			]);
+		} catch (error) {
+			console.error("Error communicating with Flask API:", error.message);
+		}
+	};
 
-  return (
-    <div>
-      <Header />
-      <div className="styles.hero-image">
-        <img src={svgImage} alt="intro" />
-      </div>
-      <div className={styles.Chatbot}>
-        <div className={styles["response-area"]}>
-          {messages.map((message, index) => (
-            <div
-              key={index}
-              className={`${styles.message} ${
-                message.sender === "Chatbot"
-                  ? styles["chatbot-message"]
-                  : styles["user-message"]
-              } ${message.isError ? styles["error-message"] : ""}`}
-            >
-              {message.sender === "user" && (
-                <img
-                  src={userAvatar}
-                  alt="User Avatar"
-                  className={styles["user-avatar"]}
-                />
-              )}
-              {message.sender === "Chatbot" && (
-                <img
-                  src={botAvatar}
-                  alt="Bot Avatar"
-                  className={styles["bot-avatar"]}
-                />
-              )}
-              <div className={styles.messageContent}>
-                {message.message}
-                <div className={styles.timestamp}>{message.timestamp}</div>
-              </div>
-            </div>
-          ))}
-        </div>
-        <div className={styles["prompt-area"]}>
-          <input
-            type="text"
-            placeholder="Send a message..."
-            value={input}
-            onChange={handleChange}
-            className={styles["input-box"]}
-          />
-          <button className={styles.submit} type="submit" onClick={handleSend}>
-            Send
-          </button>
-        </div>
-      </div>
-      <Footer />
-    </div>
-  );
+	return (
+		<div>
+			<Header />
+			<div className="flex p-8 min-h-fit justify-center">
+				<div className="styles.hero-image p-8 flex-1">
+					<img src={svgImage} alt="intro" className="w-96 h-80" />
+				</div>
+				<div className="flex w-full justify-center flex-1 ">
+					<div styles={styles.Chatbot} className="justify-end flex flex-col">
+						<div className={styles["response-area"]}>
+							{messages.map((message, index) => (
+								<div
+									key={index}
+									className={`${styles.message} ${
+										message.sender === "Chatbot"
+											? styles["chatbot-message"]
+											: styles["user-message"]
+									} ${
+										message.isError
+											? styles["error-message"]
+											: ""
+									}`}
+								>
+									{message.sender === "user" && (
+										<img
+											src={userAvatar}
+											alt="User Avatar"
+											className={styles["user-avatar"]}
+										/>
+									)}
+									{message.sender === "Chatbot" && (
+										<img
+											src={botAvatar}
+											alt="Bot Avatar"
+											className={styles["bot-avatar"]}
+										/>
+									)}
+									<div className={styles.messageContent}>
+										{message.message}
+										<div className={styles.timestamp}>
+											{message.timestamp}
+										</div>
+									</div>
+								</div>
+							))}
+						</div>
+						<div className={styles["prompt-area"]}>
+							<input
+								type="text"
+								placeholder="Send a message..."
+								value={input}
+								onChange={handleChange}
+								className={styles["input-box"]}
+							/>
+							<button
+								className={styles.submit}
+								type="submit"
+								onClick={handleSend}
+							>
+								Send
+							</button>
+						</div>
+					</div>
+				</div>
+			</div>
+			<Footer />
+		</div>
+	);
 }
 
 export default Chatbot;
